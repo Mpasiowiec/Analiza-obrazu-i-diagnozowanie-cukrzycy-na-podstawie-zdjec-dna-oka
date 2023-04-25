3be22f03b745c10306fa2c421b9affe6897a8aaa
Mpasiowiec <102516644+Mpasiowiec@users.noreply.github.com>
2023-04-25 21:35:00 +0200

    one

--- prep_data.py
+++ prep_data.py
@@ -1,8 +1,8 @@
-import splitfolders, sys, os
+import splitfolders, sys
 
 input = sys.argv[1]
 output = sys.argv[2]
 
 if len(sys.argv) == 3:
-    rat = (.7, .2, .1) 
+    rat = (.7, .2, .1)
 splitfolders.ratio('./ARIA/'+input, output="./"+output, seed=1337, ratio=rat)
