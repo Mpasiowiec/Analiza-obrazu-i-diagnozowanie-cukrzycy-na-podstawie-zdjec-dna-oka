561b48e12091321d457eafc18bdf759b71552b8d
Mpasiowiec <102516644+Mpasiowiec@users.noreply.github.com>
2023-04-27 18:01:03 +0200

    first model

--- prep_data.py
+++ prep_data.py
@@ -8,31 +8,35 @@ def scaleRadius(img, scale):
     return cv2.resize(img, (0,0), fx=s, fy=s)
 
 
-input = sys.argv[1]
-output = sys.argv[2]
+output = sys.argv[1]
 rat = (.7, .2, .1)
-scale = 255
+scale = 300
 
 # if len(sys.argv) == 3:
     # rat = (.7, .2, .1)
-        
-splitfolders.ratio('./ARIA/'+input, output="./"+output+"/raw_data", seed=1337, ratio=rat)
+    
+for folder in glob.glob("./ARIA/*"):
+    if folder == "./ARIA\\readme.txt" or  folder == './ARIA\\vessel': continue
+    
+    if folder == './ARIA\\raw': 
+        dil = "/raw_data" 
+        splitfolders.ratio(folder, output="./"+output+dil, seed=1337, ratio=rat)
 
-for f in glob.glob("./"+output+"/raw_data/train/control/*.tif")+glob.glob("./"+output+"/raw_data/test/control/*.tif")+glob.glob("./"+output+"/raw_data/val/control/*.tif"):
-    
-    a=cv2.imread(f)
-    a=scaleRadius(a,scale)
-    aa=a
-    a=cv2.addWeighted(a,                                   4,
-                      cv2.GaussianBlur(a,(0,0), scale/30),-4,
-                      128)
-    b=numpy.zeros(a.shape)
-    cv2.circle(b,(int(a.shape[1]/2), int(a.shape[0]/2)),
-               int(scale*0.9), (1,1,1),-1,8,0)
-    
-    a=a*b+128*(1-b)
+        for f in glob.glob("./"+output+"/raw_data/train/control/*.tif")+glob.glob("./"+output+"/raw_data/test/control/*.tif")+glob.glob("./"+output+"/raw_data/val/control/*.tif")+glob.glob("./"+output+"/raw_data/train/diabetic/*.tif")+glob.glob("./"+output+"/raw_data/test/diabetic/*.tif")+glob.glob("./"+output+"/raw_data/val/diabetic/*.tif"):
 
-    os.makedirs(os.path.join(f[:2+len(output)]+f[5+len(output):]).split('\\')[0],exist_ok = True)
-    cv2.imwrite(os.path.join(f[:2+len(output)]+f[5+len(output):-4]+'.png'),a)
-    
+            a=cv2.imread(f)
+            a=scaleRadius(a,scale)
+            a=cv2.addWeighted(a,                                   4,
+                              cv2.GaussianBlur(a,(0,0), scale/30),-4,
+                              128)
+            b=numpy.zeros(a.shape)
+            cv2.circle(b,(int(a.shape[1]/2), int(a.shape[0]/2)),
+                       int(scale*0.9), (1,1,1),-1,8,0)
+ 
+            a=a*b+128*(1-b)
+            # print(os.path.join(f[:3+len(output)]+f[7+len(output):-4]+'.png'))
+            os.makedirs(os.path.join(f[:3+len(output)]+f[7+len(output):]).split('\\')[0],exist_ok = True)
+            cv2.imwrite(os.path.join(f[:3+len(output)]+f[7+len(output):-4]+'.png'),a)
+    else:
+        dil = "/data_vessel"
     
