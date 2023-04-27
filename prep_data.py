2d51c31014213497d49191252e7ee057dd98a86d
Mpasiowiec <102516644+Mpasiowiec@users.noreply.github.com>
2023-04-27 15:27:08 +0200

    small changes

--- prep_data.py
+++ prep_data.py
@@ -1,8 +1,43 @@
 import splitfolders, sys
+import cv2 , glob , numpy, os
+
+def scaleRadius(img, scale):
+    x=img[int(img.shape[0]/2), :, :].sum(1)
+    r=(x>x.mean()/10).sum()/2
+    s=scale*1./r
+    return cv2.resize(img, (0,0), fx=s, fy=s)
+
 
 input = sys.argv[1]
 output = sys.argv[2]
+rat = (.7, .2, .1)
+scale = 255
 
-if len(sys.argv) == 3:
-    rat = (.7, .2, .1)
+# if len(sys.argv) == 3:
+    # rat = (.7, .2, .1)
+        
 splitfolders.ratio('./ARIA/'+input, output="./"+output, seed=1337, ratio=rat)
+
+for f in glob.glob("data/train/aria_c_markups/*.tif"):
+    
+    a=cv2.imread(f)
+    a=scaleRadius(a,scale)
+    aa=a
+    a=cv2.addWeighted(a,                                   4,
+                      cv2.GaussianBlur(a,(0,0), scale/30),-4,
+                      128)
+    b=numpy.zeros(a.shape)
+    cv2.circle(b,(int(a.shape[1]/2), int(a.shape[0]/2)),
+               int(scale*0.9), (1,1,1),-1,8,0)
+    # cv2.imshow("first Image",a)
+    # cv2.imshow("second Image",b)
+    
+    a=a*b+128*(1-b)
+    
+    # cv2.imshow("third Image",aa*b+128*(1-b))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    os.makedirs(os.path.join('./asdfsadf_'+f).split('\\')[0],exist_ok = True)
+    cv2.imwrite(os.path.join('asdfsadf_'+f[:-4]+'.png'),a)
+    
+    
