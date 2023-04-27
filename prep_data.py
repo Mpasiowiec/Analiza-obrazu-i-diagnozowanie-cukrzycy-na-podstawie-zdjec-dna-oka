05bdae9f39f1dbafc33f32c148575830d9fa092d
Mpasiowiec <102516644+Mpasiowiec@users.noreply.github.com>
2023-04-27 15:39:57 +0200

    Update prep_data.py

--- prep_data.py
+++ prep_data.py
@@ -16,9 +16,9 @@ scale = 255
 # if len(sys.argv) == 3:
     # rat = (.7, .2, .1)
         
-splitfolders.ratio('./ARIA/'+input, output="./"+output, seed=1337, ratio=rat)
+splitfolders.ratio('./ARIA/'+input, output="./"+output+"/raw_data", seed=1337, ratio=rat)
 
-for f in glob.glob("data/train/aria_c_markups/*.tif"):
+for f in glob.glob("./"+output+"/raw_data/train/control/*.tif")+glob.glob("./"+output+"/raw_data/test/control/*.tif")+glob.glob("./"+output+"/raw_data/val/control/*.tif"):
     
     a=cv2.imread(f)
     a=scaleRadius(a,scale)
@@ -29,15 +29,10 @@ for f in glob.glob("data/train/aria_c_markups/*.tif"):
     b=numpy.zeros(a.shape)
     cv2.circle(b,(int(a.shape[1]/2), int(a.shape[0]/2)),
                int(scale*0.9), (1,1,1),-1,8,0)
-    # cv2.imshow("first Image",a)
-    # cv2.imshow("second Image",b)
     
     a=a*b+128*(1-b)
-    
-    # cv2.imshow("third Image",aa*b+128*(1-b))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    os.makedirs(os.path.join('./asdfsadf_'+f).split('\\')[0],exist_ok = True)
-    cv2.imwrite(os.path.join('asdfsadf_'+f[:-4]+'.png'),a)
+
+    os.makedirs(os.path.join(f[:2+len(output)]+f[5+len(output):]).split('\\')[0],exist_ok = True)
+    cv2.imwrite(os.path.join(f[:2+len(output)]+f[5+len(output):-4]+'.png'),a)
     
     
